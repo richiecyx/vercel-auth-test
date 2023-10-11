678bb248fe15006b337eecdb70a0d17ccffafa0a
Richie Chan <richie@Richies-MacBook-Pro.local>
2023-10-12 01:48:28 +0800

    feat: adian

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
 import Image from 'next/image'
+import adian from './adian.png'
+import ugly from './ugly.png'
+
 import Page from "@/app/test/page";
+import {text} from "stream/consumers";
 
 export default function Home() {
 
 
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div><h1>welcome</h1></div>
+        <Image
+        src={adian}
+        width={500}
+        height={500}
+        alt="Picture of the Adian"
+        />
+
+        <Image
+            src={ugly}
+            width={500}
+            height={500}
+            alt="Picture of the Adian"
+        />
+        <div><h1 className={"text-2xl"}>ADIAN YOU ARE A GODDAMN DOGGY BF</h1></div>
     </main>
   )
 }
